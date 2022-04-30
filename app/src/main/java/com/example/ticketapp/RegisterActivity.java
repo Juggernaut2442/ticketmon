55b28e2be1e869cd75deb023320d527076aebc1b
Yasin Timur <infoyasintimur@gmail.com>
2022-04-30 15:59:17 +0300

    Cross-page links
    The registration page has been designed and the link between the login page has been created

--- app/src/main/java/com/example/ticketapp/RegisterActivity.java
+++ app/src/main/java/com/example/ticketapp/RegisterActivity.java
@@ -7,9 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +16,12 @@ public class LoginActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_login);
-
-
+        setContentView(R.layout.activity_register);
 
     }
 
-    public void gotoRegister(View view) {
-        Intent intent = new Intent(getApplicationContext(),RegisterActivity.class);
+    public void gotoLogin(View view) {
+        Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
         startActivity(intent);
-
     }
 }

c5f4eec0203d166ab1c24bc57fedc863e5ea7f11
Juggernaut2442 <juggernaut244242@gmail.com>
2022-05-23 00:24:56 +0300

    fragments for the DrawerActivity

--- app/src/main/java/com/example/ticketapp/DrawerActivity.java
+++ app/src/main/java/com/example/ticketapp/DrawerActivity.java
@@ -1,5 +1,6 @@
 package com.example.ticketapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
@@ -8,18 +9,31 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class DrawerActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class DrawerActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drawer);
+
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        if (savedInstanceState == null) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new MainFragment()).commit();
+        navigationView.setCheckedItem(R.id.nav_home);
+        }
 
         Button drawerButton = findViewById(R.id.drawerButton);
         drawerButton.setOnClickListener(new View.OnClickListener(){
@@ -29,6 +43,33 @@ public class DrawerActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MainFragment()).commit();
+                break;
+
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+                break;
+
+            case R.id.nav_support:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new SupportFragment()).commit();
+                break;
+
+            case R.id.nav_logout:
+                Toast.makeText(this, "Logged out.", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 
     @Override

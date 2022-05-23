100f55077718afede8838a990737c10a2b556d04
Juggernaut2442 <juggernaut244242@gmail.com>
2022-05-23 17:25:44 +0300

    button list for support fragment

--- app/src/main/java/com/example/ticketapp/SupportFragment.java
+++ app/src/main/java/com/example/ticketapp/SupportFragment.java
@@ -4,15 +4,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class SupportFragment extends Fragment {
+
+    ListView listView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_support, container, false);
+        View  rootView = inflater.inflate(R.layout.fragment_support, container, false);
+
+        listView = (ListView)rootView.findViewById(R.id.listbuttons);
+
+        ArrayList<String> buttonIntList = new ArrayList<>();
+
+        buttonIntList.add("Open a Ticket");
+        buttonIntList.add("Active Tickets");
+        buttonIntList.add("Live Support");
+        buttonIntList.add("F.A.Q.");
+        buttonIntList.add("Suggestions");
+
+        ArrayAdapter buttonListAdapter = new ArrayAdapter(getContext(), R.layout.button_item_list, buttonIntList);
+
+        listView.setAdapter(buttonListAdapter);
+        return rootView;
     }
 }
